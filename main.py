ae72ad8cbdbc4259893980853e9de578d991d84d
Bailey <92wolfstar@gmail.com>
2019-04-03 16:10:55 -0500

    stuffs

--- main.py
+++ main.py
@@ -1,50 +1,32 @@
-from flask import Flask, request
-from caesar import rotate_string
+from flask import Flask, request, render_template
+from caesar import encrypt as caesar_encrypt
+from vigenere import encrypt as vigenere_encrypt, decrypt as vigenere_decrypt
 
 app = Flask(__name__)
-app.config['DEBUG'] = True
 
-form = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <style>
-            form {{
-                background-color: #eee;
-                padding: 20px;
-                margin: 0 auto;
-                width: 540px;
-                font: 16px sans-serif;
-                border-radius: 10px;
-            }}
-            textarea {{
-                margin: 10px 0;
-                width: 540px;
-                height: 120px;
-            }}
-        </style>
-    </head>
-    <body>
-        <form method='POST'>
-            <label>Rotate by:</label>
-            <input type="text" name="rot" value="0" />
-            <textarea name="text">{0}</textarea>
-            <input type="submit" value="Submit Query" />            
-        </form>
-    
-    </body>
-</html>
-"""
 
-@app.route("/")
+@app.route('/')
 def index():
-    return form.format('')
+    return render_template('form.html', ciphertext='', rot=0, key='', decrypt_hide=True)
 
-@app.route("/", methods=['POST'])
+
+@app.route('/', methods=['POST'])
 def encrypt():
-    rots = int(request.form['rot'])
-    msg = request.form['text']
-    #e_msg = rotate_string(msg, rots)
-    return form.format(rotate_string(msg, rots))
+    try:
+        decrypt_pressed = bool(request.form['decrypt'])
+    except KeyError:
+        decrypt_pressed = False
+    text = request.form['plaintext']
+    rot = int(request.form['rot'])
+    key = request.form['key']
+    method = request.form['encrypt-method']
+    if not decrypt_pressed:
+        cipher = caesar_encrypt(text, rot) if method == 'caesar' else vigenere_encrypt(text, key)
+    else:
+        cipher = caesar_encrypt(text, abs(26-rot)) if method == 'caesar' \
+            else vigenere_decrypt(text, key)
+    return render_template('form.html', ciphertext=cipher, rot=rot, key=key,
+                           decrypt_hide=decrypt_pressed, last_method=method)
 
-app.run()
+
+app.run(debug=True)
