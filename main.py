42a452d0d192d5d6b54207d162e800a0d71e2797
GittingWorkDone <48811877+GittingWorkDone@users.noreply.github.com>
2019-04-21 23:51:57 -0500

    final

--- main.py
+++ main.py
@@ -1,30 +1,54 @@
-from flask import Flask, request, render_template
-from caesar import encrypt as caesar_encrypt
+from flask import Flask, request
+from caesar import rotate_string
+# above is imports
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
 
+# form set up below
+form = '''
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            form {{
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }}
+            textarea {{
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }}
+        </style>
+    </head>
+    <body>
+      <form method='POST'> 
+		<label>Rotate By: 
+		<input name="rot" type="text" value="0" />
+		</label>
+		<textarea name="text">{0}</textarea>
+		<input type="submit" value="Encrypt" />
+      </form>
+    </body>
+</html>
+'''
+
+#below will be app set up
 @app.route('/')
 def index():
-    return render_template('form.html', ciphertext='', rot=0, key='', decrypt_hide=True)
-
-
+	return form.format('')
+	
 @app.route('/', methods=['POST'])
 def encrypt():
-    try:
-        decrypt_pressed = bool(request.form['decrypt'])
-    except KeyError:
-        decrypt_pressed = False
-    text = request.form['plaintext']
-    rot = int(request.form['rot'])
-    key = request.form['key']
-    method = request.form['encrypt-method']
-    if not decrypt_pressed:
-        cipher = caesar_encrypt(text, rot)
-    else:
-        cipher = caesar_encrypt(text, abs(26-rot)) 
-    return render_template('form.html', ciphertext=cipher, rot=rot, key=key,
-                           decrypt_hide=decrypt_pressed, last_method=method)
+	rot = request.form['rot']
+	text = request.form['text']
+	message = rotate_string(text, rot)
+	return form.format(message) 
 
-
-app.run(debug=True)
+app.run()
